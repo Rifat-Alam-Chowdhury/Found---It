dd9ecd4e7c49bb59c87d3dc7b469f86c1adfae1d
rifat chowdhury <rifatalamtabs2@gmail.com>
2025-02-11 18:16:33 +0600

    cetegory added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,6 +15,15 @@ function Home() {
 
   const [posts, setPosts] = useState([]);
 
+  const categories = [
+    { name: "Electronics", icon: "📱" },
+    { name: "Personal Accessories & Documents", icon: "💼" },
+    { name: "Bags & Backpacks", icon: "🎒" },
+    { name: "Clothing & Accessories", icon: "👕" },
+    { name: "Jewelry & Watches", icon: "⌚" },
+    { name: "Miscellaneous Items", icon: "📦" },
+  ];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -89,6 +98,45 @@ function Home() {
           </Swiper>
         </div>
         {/* banner */}
+        {/* ******************************************8
+        8
+        8
+        ****
+        8
+        8
+        8
+        *
+        *
+        * 8
+        * 8
+        * 8
+        * 8
+        *  */}
+
+        {/* cetegory */}
+        <section className="text-color  mt-5">
+          <div className="">
+            <h2 className="text-2xl font-bold text-center mb-6">
+              Browser By Categories
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {categories.map((category, index) => (
+                <SpotlightCard className="bg-white border-none">
+                  <div
+                    key={index}
+                    className="flex flex-col items-center justify-center p-4  rounded-lg  cursor-pointer "
+                  >
+                    <span className="text-4xl">{category.icon}</span>
+                    <h3 className="mt-2 text-lg font-medium text-center">
+                      {category.name}
+                    </h3>
+                  </div>
+                </SpotlightCard>
+              ))}
+            </div>
+          </div>
+        </section>
+        {/* cetegory */}
 
         {/* ***********
          *
@@ -99,62 +147,71 @@ function Home() {
          * *
          * **
          * * */}
-        <div>
-          {/* recenlost */}
-          <section className=" text-color mt-4">
-            <div className=" p-2 rounded-md bg-white">
-              <h1 className="font-semibold ml-2">Just Now</h1>
-              <div className=" lg:grid lg:grid-cols-3 gap-3">
-                {posts?.map((recent, index) => (
-                  <>
-                    <div
-                      key={index}
-                      className=" lg:hidden   flex items-center p-2 mb-2 border-b border-gray-400"
-                    >
-                      <div className="flex-1">
-                        <h1 className="mb-1">{recent.name}</h1>{" "}
-                        <p>{recent.item}</p>
-                      </div>
 
-                      <div className="flex-shrink-0 ml-4">
-                        <img
-                          src={recent.img}
-                          alt={recent.item}
-                          className="w-16 h-16 object-cover rounded-2xl"
-                        />
-                      </div>
+        {/* recenlost */}
+        <section className=" text-color mt-4">
+          <div className=" p-2 rounded-md bg-white">
+            <h1 className="font-semibold ml-2">Just Now</h1>
+            <div className=" lg:grid lg:grid-cols-3 gap-3">
+              {posts?.map((recent, index) => (
+                <>
+                  <div
+                    key={index}
+                    className=" lg:hidden   flex items-center p-2 mb-2 border-b border-gray-400"
+                  >
+                    <div className="flex-1">
+                      <h1 className="mb-1">{recent.name}</h1>{" "}
+                      <p>{recent.item}</p>
                     </div>
 
-                    <SpotlightCard className="hidden border-none  lg:block bg-white relative overflow-hidden">
-                      <figure className="relative w-full  ">
-                        <img
-                          src={recent.img}
-                          alt={recent.item}
-                          className=" h-40 w-full object-cover rounded-md"
-                        />
-                      </figure>
-                      <div className=" p-4 h-1/2 flex flex-col">
-                        <h2 className="card-title text-lg">{recent.name}</h2>
-                        <p className="text-sm line-clamp-3 mb-2">
-                          {recent.item}
-                        </p>
-                        <div className="card-actions mt-auto">
-                          <button className=" border-b-2  px-2 text-color hover:bg-purple-300 hover:text-white transition-all duration-300 ease-in-out rounded-md">
-                            View more
-                          </button>
-                        </div>
+                    <div className="flex-shrink-0 ml-4">
+                      <img
+                        src={recent.img}
+                        alt={recent.item}
+                        className="w-16 h-16 object-cover rounded-2xl"
+                      />
+                    </div>
+                  </div>
+
+                  <SpotlightCard className="hidden border-none  lg:block bg-white relative overflow-hidden">
+                    <figure className="relative w-full  ">
+                      <img
+                        src={recent.img}
+                        alt={recent.item}
+                        className=" h-40 w-full object-cover rounded-md"
+                      />
+                    </figure>
+                    <div className=" p-4 h-1/2 flex flex-col">
+                      <h2 className="card-title text-lg">{recent.name}</h2>
+                      <p className="text-sm line-clamp-3 mb-2">{recent.item}</p>
+                      <div className="card-actions mt-auto">
+                        <button className=" border-b-2  px-2 text-color hover:bg-purple-300 hover:text-white transition-all duration-300 ease-in-out rounded-md">
+                          View more
+                        </button>
                       </div>
-                    </SpotlightCard>
-                  </>
-                ))}
-              </div>
-              <div className="text-center cursor-pointer  hover:scale-125 bg-purple-100 rounded-xl  w-25 mx-auto border-b px-2 transition-transform duration-300 ease-in-out">
-                View More
-              </div>
+                    </div>
+                  </SpotlightCard>
+                </>
+              ))}
             </div>
-          </section>
-          {/* recenlost */}
-        </div>
+            <div className="text-center cursor-pointer  hover:scale-125 bg-purple-100 rounded-xl  w-25 mx-auto border-b px-2 transition-transform duration-300 ease-in-out">
+              View More
+            </div>
+          </div>
+        </section>
+        {/* recenlost */}
+
+        {/* ***************************************************************
+         *******************************************************************
+         *****************************************************************
+         *********************************************************************/}
+        {/* Community Heroes */}
+        <section></section>
+        {/* Community Heroes */}
+        {/* ***************************************************************
+         *******************************************************************
+         *****************************************************************
+         *********************************************************************/}
       </div>
     </>
   );
